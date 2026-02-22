6eadc1d4000dedd449a4ee9d9ccc572fa6ea2621
Gregoire Gentil <gregoire@gentil.com>
2026-02-22 12:43:53 -0800

    Update version of module Ente

--- private/modules/install/ente.sh
+++ private/modules/install/ente.sh
@@ -3,5 +3,6 @@
 cd /usr/local/modules/ente
 cd server
 go mod tidy
-go build cmd/museum/main.go
+go build -o museum cmd/museum/main.go
 ln -sf /disk/admin/modules/ente/museum.yaml
+go build -o keys tools/gen-random-keys/main.go
