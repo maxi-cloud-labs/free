f48c5a56f6049ad40eb7e74bedae125d99274a84
Gregoire Gentil <gregoire@gentil.com>
2026-02-24 02:01:06 -0800

    Split and improve Ente module

--- private/modules/install/ente.sh
+++ private/modules/install/ente.sh
@@ -6,3 +6,14 @@ go mod tidy
 go build -o museum cmd/museum/main.go
 ln -sf /disk/admin/modules/ente/museum.yaml
 go build -o keys tools/gen-random-keys/main.go
+cd ../web
+apt-get -y install -t trixie-backports libstd-rust-dev-wasm32
+yes | yarn install
+yarn build:accounts
+yarn build:auth
+yarn build:cast
+yarn build:embed
+yarn build:ensu
+yarn build:payments
+yarn build:photos
+yarn build:share
--- private/modules/reset/ente.sh
+++ private/modules/reset/ente.sh
@@ -1,15 +1,77 @@
 #!/bin/sh
 
-if [ "$(id -u)" = "0" ]; then
-	echo "You should not be root"
+if [ "$(id -u)" != "0" ]; then
+	echo "You need to be root"
 	exit 0
 fi
 
 echo "#Reset ente##################"
+dbpass=$(pwgen -B -c -y -n -r "\"\!\'\`\$@~#%^&*()+={[}]|:;<>?/" 12 1)
+read -r PORTaccounts PORTauth PORTcast PORTembed PORTensu PORTphotos PORTshare << EOF
+	$(jq ".enteaccounts.localPort,.enteauth.localPort,.entecast.localPort,.enteembed.localPort,.enteensu.localPort,.entephotos.localPort,.enteshare.localPort" /usr/local/modules/_core_/web/assets/modulesdefault.json | xargs)
+EOF
+read -r MINIOACCESS MINIOSECRET << EOF
+	$(jq -r ".accessKey,.secretKey" /disk/admin/modules/_config_/minio.json | xargs)
+EOF
+
 systemctl stop ente.service
 rm -rf /disk/admin/modules/ente
 mkdir /disk/admin/modules/ente
-cp /usr/local/modules/ente/server/config/example.yaml /disk/admin/modules/ente
+
+export PGPASSWORD=`jq -r .password /disk/admin/modules/_config_/postgresql.json`
+su postgres -c "psql" << EOF
+DROP DATABASE IF EXISTS entedb;
+CREATE DATABASE entedb;
+DROP USER IF EXISTS enteuser;
+CREATE USER enteuser WITH ENCRYPTED PASSWORD '${dbpass}';
+GRANT ALL PRIVILEGES ON DATABASE entedb TO enteuser;
+\c entedb
+GRANT ALL PRIVILEGES ON SCHEMA public TO enteuser;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO enteuser;
+GRANT CREATE ON SCHEMA public TO enteuser;
+\q
+EOF
+unset PGPASSWORD
+
+su admin sh -c 'mc rb --force local/ente'
+su admin sh -c 'mc mb local/ente'
+
+cat > /disk/admin/modules/ente/museum.yaml << EOF
+http:
+    port: 3200
+
+db:
+    host: localhost
+    port: 5432
+    name: entedb
+    user: enteuser
+    password: $dbpass
+
+s3:
+    are_local_buckets: true
+    use_path_style_urls: true
+    b2-eu-cen:
+      key: ${MINIOACCESS}
+      secret: ${MINIOSECRET}
+      endpoint: localhost:9000
+      region: us-east-1
+      bucket: ente
+
+apps:
+    accounts: http://localhost:$PORTaccounts
+    auth: http://localhost:$PORTauth
+    cast: http://localhost:$PORTcast
+    embed: http://localhost:$PORTembed
+    ensu: http://localhost:$PORTensu
+    photos: http://localhost:$PORTphotos
+    share: http://localhost:$PORTshare
+EOF
+
+/usr/local/modules/ente/server/keys >> /disk/admin/modules/ente/museum.yaml
+
+echo "{\"dbname\":\"entedb\", \"dbuser\":\"enteuser\", \"dbpass\":\"${dbpass}\"}" > /disk/admin/modules/_config_/ente.json
+chown admin:admin /disk/admin/modules/_config_/piped.json
+
 systemctl start ente.service
 systemctl enable ente.service
 
