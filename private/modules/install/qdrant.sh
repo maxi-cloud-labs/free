969149ea220433f2c077c38a21b063739f5cda09
Gregoire Gentil <gregoire@gentil.com>
2026-02-23 23:39:14 -0800

    Upgrade to latest for module Qdrant

--- private/modules/install/qdrant.sh
+++ private/modules/install/qdrant.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 cd /home/ai/build
-wget -nv https://github.com/qdrant/qdrant/releases/download/v1.14.1/qdrant-aarch64-unknown-linux-musl.tar.gz
+wget -nv https://github.com/qdrant/qdrant/releases/download/v1.17.0/qdrant-aarch64-unknown-linux-musl.tar.gz
 tar -xpf qdrant-aarch64-unknown-linux-musl.tar.gz
 chmod a+x qdrant
 mv qdrant /usr/local/bin/qdrant
