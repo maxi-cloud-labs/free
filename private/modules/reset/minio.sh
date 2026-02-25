c6ecd4471cf91e88c422eacafc4cd7c637ea8f9d
Gregoire Gentil <gregoire@gentil.com>
2026-02-24 21:34:55 -0800

    Clean up reset procedure of module MinIO

--- private/modules/reset/minio.sh
+++ private/modules/reset/minio.sh
@@ -12,9 +12,13 @@ PASSWORD=$(pwgen -B -c -y -n -r "\"\!\'\`\$@~#%^&*()+={[}]|:;<>?/" 12 1)
 systemctl stop minio.service
 rm -rf /disk/admin/modules/minio
 mkdir /disk/admin/modules/minio
-cat > /disk/admin/modules/minio/minio.env << EOF
+mkdir /disk/admin/modules/minio/data
+mkdir /disk/admin/modules/minio/config
+cat > /disk/admin/modules/minio/env << EOF
 MINIO_ROOT_USER=${CLOUDNAME}
 MINIO_ROOT_PASSWORD=${PASSWORD}
+MINIO_CONFIG_DIR=/disk/admin/modules/minio/config
+MINIO_VOLUMES=/disk/admin/modules/minio/data
 EOF
 
 echo "{\"accessKey\":\"${CLOUDNAME}\", \"secretKey\":\"${PASSWORD}\"}" > /disk/admin/modules/_config_/minio.json
