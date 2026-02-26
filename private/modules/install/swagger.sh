62c8553efd2e79c81fa6e65088afe22277e31f47
Gregoire Gentil <gregoire@gentil.com>
2026-02-26 14:38:29 -0800

    Add module Swagger

---
+++ private/modules/install/swagger.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+wget -q --show-progress --progress=bar:force:noscroll https://repo1.maven.org/maven2/io/swagger/codegen/v3/swagger-codegen-cli/3.0.78/swagger-codegen-cli-3.0.78.jar -O /usr/local/bin/swagger-codegen-cli.jar
