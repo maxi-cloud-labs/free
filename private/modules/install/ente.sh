612f9a5a85678e2e7ddbf4a812f25befab19786e
Gregoire Gentil <gregoire@gentil.com>
2026-02-27 02:12:37 -0800

    Fix backend url for all Ente * modules

--- private/modules/install/ente.sh
+++ private/modules/install/ente.sh
@@ -10,12 +10,24 @@ cd ../web
 apt-get -y install -t trixie-backports libstd-rust-dev-wasm32
 yes | yarn install
 yarn build:accounts
+find apps/accounts/out -name "*.js" -exec sed -i 's/[a-zA-Z0-9]\+.env.NEXT_PUBLIC_ENTE_ENDPOINT/window.env.NEXT_PUBLIC_ENTE_ENDPOINT/g' {} \;
+find apps/accounts/out -name "*.html" -exec sed -i '/<head>/a <script>window.env = { NEXT_PUBLIC_ENTE_ENDPOINT: window.location.protocol + "//" + window.location.hostname.replace("enteaccounts.", "ente.") };</script>' {} \;
 yarn build:auth
+find apps/auth/out -name "*.js" -exec sed -i 's/[a-zA-Z0-9]\+.env.NEXT_PUBLIC_ENTE_ENDPOINT/window.env.NEXT_PUBLIC_ENTE_ENDPOINT/g' {} \;
+find apps/auth/out -name "*.html" -exec sed -i '/<head>/a <script>window.env = { NEXT_PUBLIC_ENTE_ENDPOINT: window.location.protocol + "//" + window.location.hostname.replace("enteauth.", "ente.") };</script>' {} \;
 yarn build:cast
+find apps/cast/out -name "*.js" -exec sed -i 's/[a-zA-Z0-9]\+.env.NEXT_PUBLIC_ENTE_ENDPOINT/window.env.NEXT_PUBLIC_ENTE_ENDPOINT/g' {} \;
+find apps/cast/out -name "*.html" -exec sed -i '/<head>/a <script>window.env = { NEXT_PUBLIC_ENTE_ENDPOINT: window.location.protocol + "//" + window.location.hostname.replace("entecast.", "ente.") };</script>' {} \;
 yarn build:embed
+find apps/embed/out -name "*.js" -exec sed -i 's/[a-zA-Z0-9]\+.env.NEXT_PUBLIC_ENTE_ENDPOINT/window.env.NEXT_PUBLIC_ENTE_ENDPOINT/g' {} \;
+find apps/embed/out -name "*.html" -exec sed -i '/<head>/a <script>window.env = { NEXT_PUBLIC_ENTE_ENDPOINT: window.location.protocol + "//" + window.location.hostname.replace("enteembed.", "ente.") };</script>' {} \;
 yarn build:ensu
+find apps/ensu/out -name "*.js" -exec sed -i 's/[a-zA-Z0-9]\+.env.NEXT_PUBLIC_ENTE_ENDPOINT/window.env.NEXT_PUBLIC_ENTE_ENDPOINT/g' {} \;
+find apps/ensu/out -name "*.html" -exec sed -i '/<head>/a <script>window.env = { NEXT_PUBLIC_ENTE_ENDPOINT: window.location.protocol + "//" + window.location.hostname.replace("enteensu.", "ente.") };</script>' {} \;
 yarn build:payments
 yarn build:photos
+find apps/photos/out -name "*.js" -exec sed -i 's/[a-zA-Z0-9]\+.env.NEXT_PUBLIC_ENTE_ENDPOINT/window.env.NEXT_PUBLIC_ENTE_ENDPOINT/g' {} \;
+find apps/photos/out -name "*.html" -exec sed -i '/<head>/a <script>window.env = { NEXT_PUBLIC_ENTE_ENDPOINT: window.location.protocol + "//" + window.location.hostname.replace("entephotos.", "ente.") };</script>' {} \;
 yarn build:share
 cd ../cli
 go mod tidy
