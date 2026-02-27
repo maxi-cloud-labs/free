a6dd388e425f2ac80d53b7418600a96d7860430c
Gregoire Gentil <gregoire@gentil.com>
2026-02-27 02:09:34 -0800

    Add clean-up at end of installation for module Typesense Dashboard

--- private/modules/install/typesensedashboard.sh
+++ private/modules/install/typesensedashboard.sh
@@ -4,4 +4,12 @@ cd /usr/local/modules/typesensedashboard
 npm install
 ./node_modules/.bin/quasar prepare
 PUBLIC_PATH=./ ./node_modules/.bin/quasar build
+RET=$?
+if [ $RET = 0 ]; then
+	echo "**CLEANING**"
+	mv dist/spa /tmp/typesensedashboard
+	cd /usr/local/modules/
+	rm -rf /usr/local/modules/typesensedashboard/
+	mv /tmp/typesensedashboard /usr/local/modules/
+fi
 ln -sf /disk/admin/modules/typesensedashboard/config.json /usr/local/modules/typesensedashboard/dist/spa/
