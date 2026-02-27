a94c13633f81f4aa288a7e9792f251f185421499
Gregoire Gentil <gregoire@gentil.com>
2026-02-27 02:06:49 -0800

    Add clean-up at end of installation for module Shields

--- private/modules/install/shields.sh
+++ private/modules/install/shields.sh
@@ -5,3 +5,8 @@ npm ci
 sync
 echo 3 > /proc/sys/vm/drop_caches
 NODE_OPTIONS="--max-old-space-size=2048" npm run build
+RET=$?
+if [ $RET = 0 ]; then
+	echo "**CLEANING**"
+	npm prune --production
+fi
