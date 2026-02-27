f182e24495d3201f548c2f24bc59fbbf77dcfbd1
Gregoire Gentil <gregoire@gentil.com>
2026-02-27 02:08:36 -0800

    Make compile module New API

--- private/modules/install/newapi.sh
+++ private/modules/install/newapi.sh
@@ -3,7 +3,11 @@
 cd /usr/local/modules/newapi
 cd web
 bun install
+sync
+echo 3 > /proc/sys/vm/drop_caches
 NODE_OPTIONS="--max-old-space-size=4096" bun run build
+sync
+echo 3 > /proc/sys/vm/drop_caches
 cd ..
 go build -o /usr/local/bin/newapi
 RET=$?
