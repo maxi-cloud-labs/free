e87008db207f9d5a1b295cbd5897411c4a4bd60e
Gregoire Gentil <gregoire@gentil.com>
2026-02-27 02:07:32 -0800

    Add clean-up at end of installation for module New API

--- private/modules/install/newapi.sh
+++ private/modules/install/newapi.sh
@@ -6,5 +6,9 @@ bun install
 NODE_OPTIONS="--max-old-space-size=4096" bun run build
 cd ..
 go build -o /usr/local/bin/newapi
-cd /home/ai
-#rm -rf /usr/local/modules/newapi
+RET=$?
+if [ $RET = 0 ]; then
+	echo "**CLEANING**"
+	cd /home/ai
+	rm -rf /usr/local/modules/newapi/
+fi
