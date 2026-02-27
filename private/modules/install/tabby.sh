894f7b04e7bd5a9ee69fdf175506f99447852468
Gregoire Gentil <gregoire@gentil.com>
2026-02-27 02:13:47 -0800

    Add clean-up at end of installation for module Tabby

--- private/modules/install/tabby.sh
+++ private/modules/install/tabby.sh
@@ -5,8 +5,13 @@ cd /usr/local/modules/tabby
 sync
 echo 3 > /proc/sys/vm/drop_caches
 cargo build --release
+RET=$?
+if [ $RET = 0 ]; then
+	echo "**CLEANING**"
+	cp /usr/local/modules/tabby/target/release/llama-server /usr/local/bin
+	cp /usr/local/modules/tabby/target/release/tabby /usr/local/bin
+	cd /home/ai
+	rm -rf /usr/local/modules/tabby/
+fi
 sync
 echo 3 > /proc/sys/vm/drop_caches
-cp /usr/local/modules/tabby/target/release/llama-server /usr/local/bin
-cp /usr/local/modules/tabby/target/release/tabby /usr/local/bin
-rm -rf /usr/local/modules/tabby/target/
