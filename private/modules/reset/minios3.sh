de82b1309a64fb220cdbb83bfec3d0624a5656e6
Gregoire Gentil <gregoire@gentil.com>
2026-03-01 16:07:47 -0800

    Add module MinIO S3 to expose properly MinIO backend

---
+++ private/modules/reset/minios3.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+if [ "$(id -u)" = "0" ]; then
+	echo "You should not be root"
+	exit 0
+fi
+
+echo "#Reset minios3##################"
+echo "{ \"a\":\"status\", \"module\":\"$(basename $0 .sh)\", \"state\":\"finish\" }" | websocat -1 ws://localhost:8094
