4a7a07a1c7e9ab9dcef17ab1b71c4faf780af344
Gregoire Gentil <gregoire@gentil.com>
2026-03-03 01:58:31 -0800

    Add loopback for smtp domain name in image

--- private/modules/reset/postfix.sh
+++ private/modules/reset/postfix.sh
@@ -8,6 +8,7 @@ fi
 echo "#Reset postfix##################"
 CLOUDNAME=$(jq -r ".info.name" /disk/admin/modules/_config_/_cloud_.json)
 echo "${CLOUDNAME}.mydongle.cloud" > /etc/mailname
+echo "127.0.0.1	smtp.${CLOUDNAME}.mydongle.cloud" >> /etc/hosts
 sed -i -e "s|^myhostname =.*|myhostname = smtp.${CLOUDNAME}.mydongle.cloud|" /etc/postfix/main.cf
 
 echo "{ \"a\":\"status\", \"module\":\"$(basename $0 .sh)\", \"state\":\"finish\" }" | websocat -1 ws://localhost:8094
