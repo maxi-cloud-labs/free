e2de73ad0be98601c7acd95c0eaf63c3ee5e71a5
Gregoire Gentil <gregoire@gentil.com>
2026-03-10 16:06:01 -0700

    Make coherent path for Python environment in module MeTube

--- private/modules/install/metube.sh
+++ private/modules/install/metube.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 cd /usr/local/modules/metube
-/home/ai/rootfs/usr/local/modules/_core_/pip.sh -f /usr/local/modules/metube/env -s
+/home/ai/rootfs/usr/local/modules/_core_/pip.sh -f /usr/local/modules/metube/penv -s
 echo "PATH before any modif: $PATH"
 PATHOLD=$PATH
-PATH=/usr/local/modules/metube/env/bin:$PATHOLD
-export PATH=/usr/local/modules/metube/env/bin:$PATHOLD
+PATH=/usr/local/modules/metube/penv/bin:$PATHOLD
+export PATH=/usr/local/modules/metube/penv/bin:$PATHOLD
 echo "PATH new: $PATH python: `python --version`"
 pip install aiohttp yt_dlp mutagen curl-cffi watchfiles python-socketio
 PATH=$PATHOLD
