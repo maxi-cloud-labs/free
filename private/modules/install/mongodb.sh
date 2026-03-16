16315fec16b7c26135fdb300daca36100c3cb67b
Gregoire Gentil <gregoire@gentil.com>
2026-03-15 21:38:09 -0700

    Make coherent install script for MongoDB module

--- private/modules/install/mongodb.sh
+++ private/modules/install/mongodb.sh
@@ -3,7 +3,7 @@
 curl -fsSL https://www.mongodb.org/static/pgp/server-8.0.asc | sudo gpg --dearmor -o /etc/apt/keyrings/mongodb.gpg
 echo "deb [arch=arm64 signed-by=/etc/apt/keyrings/mongodb.gpg] https://repo.mongodb.org/apt/ubuntu noble/mongodb-org/8.2 multiverse" > /etc/apt/sources.list.d/mongodb.list
 apt-get update
-apt-get install -y mongodb-org
+apt-get -y install mongodb-org
 sed -i -e "s|^  dbPath:.*|  dbPath: /disk/admin/modules/mongodb|" /etc/mongod.conf
 echo "security.authorization: enabled" >> /etc/mongod.conf
 rm -f /usr/lib/systemd/system/mongod.service
