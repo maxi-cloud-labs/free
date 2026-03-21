ff605416cb91444ccf760152ecae7c302d7350bd
Gregoire Gentil <gregoire@gentil.com>
2026-03-20 20:02:17 -0700

    Improve security of module Apache2

--- private/modules/install/apache2.sh
+++ private/modules/install/apache2.sh
@@ -3,3 +3,5 @@
 apt-get -y install apache2
 rm -f /etc/apache2/sites-enabled/*
 rm -f /etc/apache2/ports.conf
+sed -i -e "s/^ServerTokens .*/ServerTokens Prod/" /etc/apache2/conf-enabled/security.conf
+sed -i -e "s/^ServerSignature .*/ServerSignature Off/" /etc/apache2/conf-enabled/security.conf
