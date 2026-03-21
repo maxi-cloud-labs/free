6650b4f371cede2b8da2f117c914297b065840ba
Gregoire Gentil <gregoire@gentil.com>
2026-03-20 22:52:58 -0700

    Improve toast when multiple occurs in client app

--- client/src/app/components/toast/toast.component.ts
+++ client/src/app/components/toast/toast.component.ts
@@ -16,6 +16,7 @@ instant: boolean = false;
 message: string = "";
 icon: string = "";
 delay;
+autoHideTimer = null;
 
 constructor(public global: Global, private cdr: ChangeDetectorRef) {
 	global.toast.subscribe(event => {
@@ -28,23 +29,41 @@ constructor(public global: Global, private cdr: ChangeDetectorRef) {
 }
 
 dismissToast(now) {
-	if (now)
-		this.hide = true;
-	else
-	    setTimeout(() => { this.hide = true; }, 1000);
 	this.show = false;
     this.removeEscapeListener();
+	if (now) {
+		this.hide = true;
+		if (this.autoHideTimer) {
+			clearTimeout(this.autoHideTimer);
+			this.autoHideTimer = null;
+		}
+	} else {
+	    setTimeout(() => {
+			this.hide = true;
+			if (this.autoHideTimer) {
+				clearTimeout(this.autoHideTimer);
+				this.autoHideTimer = null;
+			}
+		}, 1000);
+	}
 }
 
-showToast(message, icon, delay) {
+async showToast(message, icon, delay) {
+	if (this.autoHideTimer) {
+        setTimeout(() => this.showToast(message, icon, delay), 1000);
+        return;
+	}
 	this.delay = delay;
 	this.message = message;
 	this.icon = icon;
 	this.instant = false;
 	this.hide = false;
-    setTimeout(() => { this.show = true; }, 10);
+	this.cdr.detectChanges();
+    setTimeout(() => { this.show = true; this.cdr.detectChanges(); }, 10);
 	if (delay > 0)
-	    setTimeout(() => { this.dismissToast(false); }, delay);
+	    this.autoHideTimer = setTimeout(() => { this.dismissToast(false); }, delay);
+	else
+		this.autoHideTimer = null;
     this.addEscapeListener();
 }
 
