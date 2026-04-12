f2eca1b916fda749a879cb7e39adc17dc608ac40
Gregoire Gentil <gregoire@gentil.com>
2026-04-11 23:30:29 -0700

    Set the setup of module DomainMOD

--- private/modules/reset/domainmod.sh
+++ private/modules/reset/domainmod.sh
@@ -36,7 +36,23 @@ cat > /disk/admin/modules/domainmod/config.inc.php << EOF
 \$dbpassword = '${DBPASSM}';
 EOF
 
-echo "{\"email\":\"${EMAIL}\", \"username\":\"${CLOUDNAME}\", \"password\":\"${PASSWD}\", \"dbname\":\"${dbname}\", \"dbuser\":\"${dbuser}\", \"dbpass\":\"${DBPASSM}\"}" > /disk/admin/modules/_config_/domainmod.json
+COOKIE_JAR=$(mktemp)
+PORT=`jq -r .domainmod.localPort /usr/local/modules/_core_/web/assets/modulesdefault.json`
+response=`curl -sS -L -b "$COOKIE_JAR" -c "$COOKIE_JAR" -X POST http://localhost:$PORT/install/language/index.php -d new_language=en_US.UTF-8`
+response=`curl -sS -L -b "$COOKIE_JAR" -c "$COOKIE_JAR" -X POST http://localhost:$PORT/install/requirements/index.php`
+response=`curl -sS -L -b "$COOKIE_JAR" -c "$COOKIE_JAR" -X POST http://localhost:$PORT/install/currency/index.php -d new_currency=USD`
+response=`curl -sS -L -b "$COOKIE_JAR" -c "$COOKIE_JAR" -X POST http://localhost:$PORT/install/timezone/index.php -d new_timezone=America/Los_Angeles`
+response=`curl -sS -L -b "$COOKIE_JAR" -c "$COOKIE_JAR" -X POST http://localhost:$PORT/install/email-admin/index.php -d new_admin_email1=$EMAIL -d new_admin_email2=$EMAIL`
+response=`curl -sS -L -b "$COOKIE_JAR" -c "$COOKIE_JAR" -X POST http://localhost:$PORT/install/email-system/index.php -d new_system_email1=$EMAIL -d new_system_email2=$EMAIL`
+rm -f COOKIE_JAR
+
+HASH=$(php -r "echo password_hash('$PASSWD', PASSWORD_BCRYPT, ['cost' => 12]);")
+mysql --defaults-file=/disk/admin/modules/mysql/conf.txt << EOF
+USE domainmodDB;
+UPDATE users SET new_password=0, password='$HASH' where id=1;
+EOF
+
+echo "{\"email\":\"${EMAIL}\", \"username\":\"admin\", \"password\":\"${PASSWD}\", \"dbname\":\"${dbname}\", \"dbuser\":\"${dbuser}\", \"dbpass\":\"${DBPASSM}\"}" > /disk/admin/modules/_config_/domainmod.json
 chown admin:admin /disk/admin/modules/_config_/domainmod.json
 
 chown -R admin:admin /disk/admin/modules/domainmod
